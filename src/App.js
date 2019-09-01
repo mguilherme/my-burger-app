b297ec078fc0ce91e5c4f7ba151379f7271b36ae
Miguel Guilherme <mguilherme82@gmail.com>
2019-09-01 15:17:22 +0100

    153. Start Implementation of The Burger Builder Container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from "./components/layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 const App = () => (
     <div>
         <Layout>
-            <p>Test</p>
+            <BurgerBuilder/>
         </Layout>
     </div>
 );
