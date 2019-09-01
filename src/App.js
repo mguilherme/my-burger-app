84348e4d784c26c70b28d87855c8aa9ad0ad201e
Miguel Guilherme <mguilherme82@gmail.com>
2019-09-01 14:25:20 +0100

    152. Creating a Layout Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Layout from "./components/layout/Layout";
 
-const App = () => {
-    return (
-        <div>
-            <h1>App.js</h1>
-        </div>
-    );
-}
+const App = () => (
+    <div>
+        <Layout>
+            <p>Test</p>
+        </Layout>
+    </div>
+);
 
 export default App;
